07686dd91a8521a0eb53fe058d5f976c77a49fd7
Michael Muckler <michaelmuckler@Michaels-MBP.localdomain>
2018-02-05 18:09:13 -0600

    First Day Homework

---
+++ projects/tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,4 @@
+game_quest = input("Do you want to play a game")
+while game_quest != "yes" and game_quest !="no"
+  game_quest = input("Again do you want to play a game")
+print ("Ok let's get started")
